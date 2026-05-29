4a06ef8ef7f3b587c10bcdc913e6b9657391a4ab
Stiven Giraldo <giraldostiven150@gmail.com>
2026-05-28 20:34:40 -0500

    Se corrige url de navbar

--- src/controllers/admin/navbar/navbarAdminTemplate.js
+++ src/controllers/admin/navbar/navbarAdminTemplate.js
@@ -6,9 +6,9 @@ export const navbarAdminTemplate = () => `
                 <h1 class="text-2xl font-cormorant text-white">Catálogo local</h1>
             </div>
             <div class="flex items-center gap-3">
-                <a href="/src/pages/account/myProfile.html"
+                <a href="../../../pages/account/myProfile.html"
                     class="text-xs uppercase tracking-widest font-montserrat text-white/80 hover:text-white">Mi perfil</a>
-                <a href="/src/pages/home/home.html"
+                <a href="../../../pages/home/home.html"
                     class="text-xs uppercase tracking-widest font-montserrat text-white/80 hover:text-white">Ir a la tienda</a>
             </div>
         </nav>
